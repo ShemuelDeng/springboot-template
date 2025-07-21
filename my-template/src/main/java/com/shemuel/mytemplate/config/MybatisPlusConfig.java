68bb04523ac438e8b220f11d132af78d7e095e6d
dengshaoxiang <dengshaoxiang@vv.cn>
2025-07-21 18:03:48 +0800

    取消多租户插件

--- my-template/src/main/java/com/shemuel/mytemplate/config/MybatisPlusConfig.java
+++ my-template/src/main/java/com/shemuel/mytemplate/config/MybatisPlusConfig.java
@@ -9,8 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@MapperScan("com.shemuel.mytemplate.mapper")
+//@Configuration
+//@MapperScan("com.shemuel.mytemplate.mapper")
 public class MybatisPlusConfig {
 
     @Autowired
